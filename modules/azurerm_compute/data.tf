a678e075fab031b5b5f43229906b6e3c899da644
jaikumar9760 <jaikumar9760@gmail.com>
2025-12-28 16:18:17 +0530

    Delete modules directory

--- modules/azurerm_compute/data.tf
+++
@@ -1,6 +0,0 @@
-data "azurerm_subnet" "mySubnet" {
-  for_each = var.vms
-  name                 = each.value.subnet_name
-  virtual_network_name = each.value.virtual_network_name
-  resource_group_name  = each.value.resource_group_name
-}
--- modules/azurerm_resource_group/main.tf
+++
@@ -1,9 +0,0 @@
-
-resource "azurerm_resource_group" "myrg" {
-  for_each = var.rgs
-  name = each.value.resource_group_name
-  location = each.value.resource_group_location
-  managed_by = each.value.managed_by
-  tags = { for k, v in each.value.tags : k => v.value }
-
-}
